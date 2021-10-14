13c819a224663c97a7648a90c4cda5fc679fb534
M Sufyan Khan <msufyankhan709@gmail.com>
2021-10-14 19:13:24 +0500

    commit changes

--- src/components/contributions.vue
+++ src/components/contributions.vue
@@ -1,38 +1,43 @@
 <template>
-    <h2>
-         <v-breadcrumbs :items="items">
-      <template v-slot:divider>
-        <v-icon>mdi-chevron-right</v-icon>
-      </template>
-    </v-breadcrumbs>
-        <h1 style="margin-top:150px; margin-left:120px; margin-right:700px">Contributions to open source</h1>
-    </h2>
+  <h2>
+    <div class="mt-9">
+      <v-btn text>
+        <v-breadcrumbs :items="items">
+          <template v-slot:divider>
+            <v-icon>mdi-chevron-right</v-icon>
+          </template>
+        </v-breadcrumbs>
+      </v-btn>
+    </div>
+    <h1 style="margin-top:150px; margin-left:120px; margin-right:700px">
+      Contributions to open source
+    </h1>
+  </h2>
 </template>
 
 <script>
-    export default {
-        name:'contributions',
-         data: () => ({
-      items: [
-        {
-          text: 'HOME',
-          disabled: false,
-          href: '/',
-        },
-         {
-          text: 'About',
-          disabled: false,
-          href: '/about',
-        },{
-            text:'Contributions',
-            disabled:false,
-            href:'/about/contributions'
-        }
-      ],
-    }),
-    }
+export default {
+  name: "contributions",
+  data: () => ({
+    items: [
+      {
+        text: "HOME",
+        disabled: false,
+        href: "/",
+      },
+      {
+        text: "About",
+        disabled: false,
+        href: "/about",
+      },
+      {
+        text: "Contributions",
+        disabled: false,
+        href: "/about/contributions",
+      },
+    ],
+  }),
+};
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
